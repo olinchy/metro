d690d542f5edfeaa983cbf18ca3f4cb9562fdb51
liuwei <liu.wei91@zte.com.cn>
2017-09-14 05:38:59 +0200

    feature : archetype for module
    Change-Id: I775f245dfd45a998f128a86f77e46ddd587fcbec

---
+++ apps/infrastructure/archetype/README.md
@@ -0,0 +1,12 @@
+#### Generate archetype
+```
+mvn clean install
+```
+
+#### create module with it
+```
+mvn archetype:generate -DarchetypeGroupId=com.zte.sdn.mw \
+-DarchetypeArtifactId=module-archetype -DarchetypeCatalog=local \
+-DarchetypeVersion=1.0.0-snapshot
+
+```
--- apps/infrastructure/archetype/src/main/resources/archetype-resources/__rootArtifactId__-impl/src/main/java/__packageInPathFormat__/impl/__classPrefix__Provider.java
+++ apps/infrastructure/archetype/src/main/resources/archetype-resources/__rootArtifactId__-impl/src/main/java/__packageInPathFormat__/impl/__classPrefix__Provider.java
@@ -2,12 +2,12 @@
 #set( $symbol_dollar = '$' )
 #set( $symbol_escape = '\' )
 /*
-* ${copyright} and others.  All rights reserved.
-*
-* This program and the accompanying materials are made available under the
-* terms of the Eclipse Public License v1.0 which accompanies this distribution,
-* and is available at http://www.eclipse.org/legal/epl-v10.html
-*/
+ * ${copyright} and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
 package ${package}.impl;
 
 import org.slf4j.Logger;
